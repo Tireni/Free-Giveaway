05ffc331d389d78e14c6242b33e2c20834e28fc3
Tireni <35170595+Tireni@users.noreply.github.com>
2018-10-21 08:17:34 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+Free Giveaway
+</head>
+<body>
+<?php
+echo "<p>Congratulations you have entered your name,address,email<br /> Just Email tirenienoch@gmail.com write your name,address,email
+and item you want and you will get it in 3 days<br /> Make sure you are the first person to email or you will not get it.<br /> Winners will get free things for 
+3 days.<br /> Thank you for Submiting your information </p>"
+
+>
+</body>
+</html>
